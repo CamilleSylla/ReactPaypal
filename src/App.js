19840d8375b868ef6d24b9ae2f8500afe0fa132b
Camille Sylla <camillesylla.web@gmail.com>
2020-11-03 04:44:51 +0100

    full app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Paypal from './components/PayPal';
+import { useState } from 'react';
+import React from 'react';
 
 function App() {
+
+  const [checkout,setCheckOut] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {checkout ? (
+        <Paypal />
+      ) : (
+          <button
+            onClick={() => {
+              setCheckOut(true);
+              console.log(checkout);
+            }}
+          >
+            Checkout
+          </button>
+        )}
+    </div >
   );
+
 }
 
 export default App;
